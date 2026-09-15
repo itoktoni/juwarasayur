c76d4d130224b58c1c649ac517252009550902f7
Itok toni - Ryzen <itok.toni@gmail.com>
2026-09-15 21:40:36 +0700

    update juwara sayur

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -2,4 +2,20 @@
 
 namespace App\Policies;
 
-class UserPolicy extends BasePolicy {}
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class UserPolicy extends BasePolicy
+{
+    // ponytail: GeneralRequest maps postApprove->can('approve') & postReject->can('reject').
+    // Tanpa method ini Gate deny -> 403 untuk semua role termasuk developer.
+    public function approve(User $user): Response
+    {
+        return $this->accessProtected($user, __FUNCTION__) ? Response::deny() : Response::allow();
+    }
+
+    public function reject(User $user): Response
+    {
+        return $this->accessProtected($user, __FUNCTION__) ? Response::deny() : Response::allow();
+    }
+}
